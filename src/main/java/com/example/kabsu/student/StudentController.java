b848ac493d12fd4ce471bdfe1b145d5d310dd50e
John Jacob Dimaya <johnjacobdimaya0@gmail.com>
2026-02-13 19:50:46 +0800

    add find,update and delete function

--- src/main/java/com/example/kabsu/student/StudentController.java
+++ src/main/java/com/example/kabsu/student/StudentController.java
@@ -25,10 +25,41 @@ public class StudentController {
                 .body(ApiResponse.success(response));
     }
 
+    @GetMapping("/{student-id}")
+    public ResponseEntity<ApiResponse<StudentResponseDto>> find(@PathVariable("student-id") Long id) {
+        var response = studentService.find(id);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(response));
+    }
+
     @GetMapping
     public ResponseEntity<ApiResponse<List<StudentResponseDto>>> findAll() {
         var response = studentService.findAll();
         return ResponseEntity.status(HttpStatus.OK)
                 .body(ApiResponse.success(response));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<ApiResponse<List<StudentResponseDto>>> findStudentByName(
+            @RequestParam(name = "firstName") String firstName) {
+        var response = studentService.findStudentByFirstName(firstName);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(response));
+    }
+
+    @PutMapping("/{student-id}")
+    public ResponseEntity<ApiResponse<StudentResponseDto>> update(
+            @PathVariable("student-id") Long studentId,
+            @Valid @RequestBody StudentUpdateDto dto) {
+        var response = studentService.update(studentId,dto);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(response));
+    }
+
+    @DeleteMapping("/{student-id}")
+    public ResponseEntity<Void> delete(@PathVariable("student-id") Long id) {
+        studentService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                .build();
     }
 }
