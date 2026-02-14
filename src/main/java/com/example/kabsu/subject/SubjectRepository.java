39603a8ffbbe5b3ef0c7d2035752637acbe2348c
John Jacob Dimaya <johnjacobdimaya0@gmail.com>
2026-02-14 12:19:09 +0800

    implements Subject Repository

--- src/main/java/com/example/kabsu/subject/SubjectRepository.java
+++ src/main/java/com/example/kabsu/subject/SubjectRepository.java
@@ -1,4 +1,9 @@
 package com.example.kabsu.subject;
 
-public interface SubjectRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubjectRepository extends JpaRepository<Subject, Long> {
+
 }
