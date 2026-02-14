d34de974cc01aac0878bf050b58666eb3019d7a0
John Jacob Dimaya <johnjacobdimaya0@gmail.com>
2026-02-14 22:30:41 +0800

    add findAllName function

--- src/main/java/com/example/kabsu/subject/SubjectRepository.java
+++ src/main/java/com/example/kabsu/subject/SubjectRepository.java
@@ -1,9 +1,12 @@
 package com.example.kabsu.subject;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
-
+    List<Subject> findAllByNameContainsIgnoreCase(String name, Pageable pageable);
 }
