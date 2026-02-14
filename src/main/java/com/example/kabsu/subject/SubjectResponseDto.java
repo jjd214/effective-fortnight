ff353e3d5674266cb238329cf9b0cc304935acaf
John Jacob Dimaya <johnjacobdimaya0@gmail.com>
2026-02-14 22:31:05 +0800

    create SubjectResponseDto

--- src/main/java/com/example/kabsu/subject/SubjectResponseDto.java
+++ src/main/java/com/example/kabsu/subject/SubjectResponseDto.java
@@ -1,4 +1,17 @@
 package com.example.kabsu.subject;
 
-public record SubjectResponseDto() {
+import com.example.kabsu.types.SubjectType;
+
+import java.time.LocalDateTime;
+
+public record SubjectResponseDto(
+        Long id,
+        String name,
+        String code,
+        String description,
+        Integer units,
+        SubjectType subjectType,
+        LocalDateTime updatedAt,
+        LocalDateTime createdAt
+) {
 }
