68b4e220f1c47afe9a49ae36aeb726fa8b320e9c
John Jacob Dimaya <johnjacobdimaya0@gmail.com>
2026-02-14 12:18:40 +0800

    add Subject entity

--- src/main/java/com/example/kabsu/subject/Subject.java
+++ src/main/java/com/example/kabsu/subject/Subject.java
@@ -1,4 +1,52 @@
 package com.example.kabsu.subject;
 
+import com.example.kabsu.student.Student;
+import com.example.kabsu.types.SubjectType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Subject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String code;
+    private String description;
+    private Integer units;
+    @Enumerated(EnumType.STRING)
+    private SubjectType type;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToMany(mappedBy = "subjects")
+    private List<Student> students = new ArrayList<>();
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = this.createdAt;
+    }
+
 }
