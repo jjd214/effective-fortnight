b41f69733a37081589e2425a25b3c86ab46f78ec
John Jacob Dimaya <johnjacobdimaya0@gmail.com>
2026-02-14 22:27:06 +0800

    created SubjectSummaryDto

--- src/main/java/com/example/kabsu/student/SubjectSummaryDto.java
+++ src/main/java/com/example/kabsu/student/SubjectSummaryDto.java
@@ -1,4 +1,16 @@
 package com.example.kabsu.student;
 
-public record SubjectSummaryDto() {
+import com.example.kabsu.types.SubjectType;
+
+import java.time.LocalDateTime;
+
+public record SubjectSummaryDto(
+        Long id,
+        String name,
+        String code,
+        String description,
+        Integer units,
+        SubjectType subjectType,
+        LocalDateTime createdAt
+) {
 }
