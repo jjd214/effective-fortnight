7e955f7356264878edcd9dfbe9f8fa7d66a42114
John Jacob Dimaya <johnjacobdimaya0@gmail.com>
2026-02-18 16:33:21 +0800

    create user request

--- src/main/java/com/example/kabsu/user/request/RegisterRequest.java
+++ src/main/java/com/example/kabsu/user/request/RegisterRequest.java
@@ -1,4 +1,27 @@
 package com.example.kabsu.user.request;
 
-public record UserRegisterRequestDto() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record RegisterRequest(
+
+        @NotBlank(message = "VALIDATION.REGISTRATION.NAME.NOT_BLANK")
+        @Size(min = 2, max = 50)
+        String name,
+
+        @NotBlank(message = "VALIDATION.REGISTRATION.EMAIL.NOT_BLANK")
+        @Email(message = "VALIDATION.REGISTRATION.EMAIL.INVALID")
+        String email,
+
+        @NotBlank(message = "VALIDATION.REGISTRATION.PASSWORD.NOT_BLANK")
+        @Size(min = 3, message = "VALIDATION.REGISTRATION.PASSWORD.SIZE")
+//        @Pattern(
+//                regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d).+$",
+//                message = "VALIDATION.REGISTRATION.PASSWORD.WEAK"
+//        )
+        String password
+
+) {
 }
